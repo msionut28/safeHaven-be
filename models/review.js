d4dccdb3b67056b37f4c7f510d82614a47a3e309
weezleb <basil.housden@yahoo.com>
2023-11-01 17:18:01 +0000

    end of wednesday merge party BE

--- models/review.js
+++ models/review.js
@@ -11,11 +11,14 @@ const reviewSchema = new mongoose.Schema({
     safety: Number,
     date: Date,
     user: String,
+    isUniversity: Boolean,
+    support: Number,
+    community: Number
 });
+
 const Reviews = mongoose.model("Review", reviewSchema);
 // review post method
 app.post("/AddReview", async (req, res) => {
-    const email = req.body.email;
     try {
         const data = req.body;
         const review = new Reviews({
@@ -24,10 +27,18 @@ app.post("/AddReview", async (req, res) => {
             inclusivity: data.inclusivity,
             safety: data.safety,
             date: data.date,
+            isUniversity: data.isUniversity,
+            support: data.support,
+            community: data.community
         });
+
+        await review.save();  
+        res.status(201).json({ message: "Review added great success" });
+
     } catch (err) {
         console.log("ERROR ruh roh zoinks scoob", err.message);
         res.status(500).json({ error: "Internal server blunder" });
     }
 });
+
 export default Reviews;
