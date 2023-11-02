47241350809c2b680178e42c4684d6e48145eac1
weezleb <basil.housden@yahoo.com>
2023-11-02 17:02:12 +0000

    end of thursday merge party

--- schemas/reviewSchema.js
+++ schemas/reviewSchema.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
 
 export const reviewSchema = new mongoose.Schema({
-    venue: String,
-    review: String,
-    inclusivity: Number,
-    safety: Number,
-    date: Date,
-    user: String,
+  venue: String,
+  review: String,
+  inclusivity: Number,
+  safety: Number,
+  date: Date,
+  user: String,
+  isUniversity: Boolean,
+  support: Number,
+  community: Number
   });
